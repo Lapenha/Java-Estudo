77b43866a1344dad754a597327fee4f8a907d6c4
Murilo Alapenha <alapenhamurilo@gmail.com>
2025-04-10 00:35:49 -0300

    commit

--- src/ArrayExemplo.java
+++ src/ArrayExemplo.java
@@ -44,6 +44,7 @@ public class ArrayExemplo {
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[i].length; j++){
                 System.out.println("Posicao ["+i+"]["+j+"] = " + matrix[i][j]);
+                //
 
             }
         }
