287cc025772888dcf22b93c894f44d3df6ddae55
Murilo Alapenha <alapenhamurilo@gmail.com>
2025-04-10 00:00:37 -0300

    first commit

---
+++ src/Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays {
+    public static void main(String[] args) {
+        
+    
+    int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int x = myArray.length;
+        System.out.print(x);
+        System.out.print(myArray);
+    }
+    
+}
