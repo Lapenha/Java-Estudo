c96477c22153e86500240e7c9268f77665837ead
Murilo Alapenha <alapenhamurilo@gmail.com>
2025-04-10 00:32:53 -0300

    estudos array

--- src/ArrayExemplo.java
+++ src/ArrayExemplo.java
@@ -15,5 +15,37 @@ public class ArrayExemplo {
         System.out.println("Primeiro numero: "+ numeros[0]);
 
         System.out.println("Tamanho do Array: " + numeros.length);
+        //for basico
+        System.out.println("Elementos com for");
+        for(int i = 0; i<numeros.length;i++){
+            System.out.println("Indice: "+ i + "-> "+ numeros[i] +"\n");
+        }
+        //for each
+        for(int num : numeros){
+            System.out.println(num);
+        }
+
+        //criando e inicialisando diretamente
+    String[] nomes = {"Ana", "Marcela", "Julia"};
+
+    System.out.println("Nomes:");
+    for(String nome : nomes) {
+        System.out.println(nome);
+    }
+
+
+    //array multidimensional
+    int[][] matrix = {
+        {1, 2},
+        {3, 4}
+    };
+        System.out.println("Matriz:");
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                System.out.println("Posicao ["+i+"]["+j+"] = " + matrix[i][j]);
+
+            }
+        }
     }
 }
