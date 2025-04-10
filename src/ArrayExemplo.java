3c67fb08ce557102981fd0c492c6d0c2a66a9f6d
Murilo Alapenha <alapenhamurilo@gmail.com>
2025-04-10 00:21:14 -0300

    Array teste

---
+++ src/ArrayExemplo.java
@@ -0,0 +1,19 @@
+
+
+public class ArrayExemplo {
+    public static void main(String[] args) {
+        //criando array de inteiros 
+        int[] numeros = new int[5];
+
+
+        numeros[0] = 10;
+        numeros[1] = 20;
+        numeros[2] = 30;
+        numeros[3] = 40;
+        numeros[4] = 50;
+        System.out.println("ARRAY");
+        System.out.println("Primeiro numero: "+ numeros[0]);
+
+        System.out.println("Tamanho do Array: " + numeros.length);
+    }
+}
