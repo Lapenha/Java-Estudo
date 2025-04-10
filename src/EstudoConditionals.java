da2229ea627de1c9d7397c8d20235092f7facfcf
Murilo Alapenha <alapenhamurilo@gmail.com>
2025-04-10 20:56:27 -0300

    commit

--- src/EstudoConditionals.java
+++ src/EstudoConditionals.java
@@ -1,6 +1,9 @@
 public class EstudoConditionals {
 
     public static void main(String[] args) {
+        int idade = 18;
         
+        String status = (idade >= 18) ? "Maior de idade" : "Menor de idade";
+        System.out.println(status);
     }
 }
